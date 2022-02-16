0886576d3ac82addf78f3e53d207074eaeec56c8
jayden van der bijl <2158035@talnet.nl>
2022-02-16 13:08:26 +0100

    opdracht2.2

---
+++ 2-Loops/Opdracht-2.2/loop_for.php
@@ -0,0 +1,6 @@
+<?php
+for ($leeftijd = 0; $leeftijd < 18; $leeftijd++){
+    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen<br>"; 
+}
+
+echo "ik ben $leeftijd jaar, dus ik heb stemrecht";
