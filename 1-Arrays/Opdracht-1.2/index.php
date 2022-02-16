8fbf6ca2630c88e52c7946b5515ea1d0150a8b32
jayden van der bijl <2158035@talnet.nl>
2022-02-16 10:23:27 +0100

    opdracht1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,11 @@
+<?php
+$student ["voornaam"] = "Willem"; 
+$student ["achternaam"] = "Alexander van Oranje"; 
+$student ["leeftijd"] = "53"; 
+$student ["opleiding"] = "ROC van Amsterdam"; 
+echo $student['voornaam'] .' '.  $student ['achternaam'] . ' van ' . $student ['leeftijd'] . ' jaar oud is een student aan het ' .  $student ['opleiding'] . ' aan de opleiding Software Ontwikkeling'; 
+
+
+
+
+
