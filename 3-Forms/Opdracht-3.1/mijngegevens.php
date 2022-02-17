ec5e97e6ccb8de241d703861eb9e37cb9ec16a4f
jayden van der bijl <2158035@talnet.nl>
2022-02-17 09:46:07 +0100

    opdracht3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,12 @@
+
+
+
+
+<form action="/action_page.php">
+<input type="text" id="name" name="name" value="piet pietersen"><br><br>
+<input type="text" id="klas" name="klas" value="9G"><br><br>
+<input type="text" id="leeftijd" name="leeftijd" value="18 jaar"><br><br>
+<input type="text" id="straat" name="straat" value="kalverstraat 11"><br><br>
+<input type="text" id="plaats" name="plaats" value="amsterdam"><br><br>
+<input type="submit" value="Submit">
+</form> 
