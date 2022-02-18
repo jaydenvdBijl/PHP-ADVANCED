35b8d7490c5b27686503bd0dd6ae375dc651c801
jayden van der bijl <2158035@talnet.nl>
2022-02-18 11:31:46 +0100

    opdracht3.1

--- 3-Forms/Opdracht-3.1/mijngegevens.php
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -1,12 +1,7 @@
+<?php
+echo "voornaam: ".$_GET['name'] . "<br>";
+echo "klas: ".$_GET['klas'] . "<br>";
+echo "leeftijd: ".$_GET['leeftijd'] . "<br>";
+echo "straat: ".$_GET['straat'] . "<br>";
+echo "plaats: ".$_GET['plaats'] . "<br>";
 
-
-
-
-<form action="/action_page.php">
-<input type="text" id="name" name="name" value="piet pietersen"><br><br>
-<input type="text" id="klas" name="klas" value="9G"><br><br>
-<input type="text" id="leeftijd" name="leeftijd" value="18 jaar"><br><br>
-<input type="text" id="straat" name="straat" value="kalverstraat 11"><br><br>
-<input type="text" id="plaats" name="plaats" value="amsterdam"><br><br>
-<input type="submit" value="Submit">
-</form> 
