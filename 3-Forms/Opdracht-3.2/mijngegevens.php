1f790cd5a5718004b75d4b788c26bcf1106f646e
jayden van der bijl <2158035@talnet.nl>
2022-03-03 09:47:43 +0100

    opdracht3.2

--- 3-Forms/Opdracht-3.2/mijngegevens.php
+++ 3-Forms/Opdracht-3.2/mijngegevens.php
@@ -1,12 +1,29 @@
-<form>
-<label for="name">wat is je lievelingskleur</label><br>
-<select name="classes">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: <?php echo $_POST["kleur"] ?>">
+
+</body>
+</html>
 <?php
-$klassen = array("yellow", "blue", "green", "purple", "red", "white", "black", "orange", "brown",);
-foreach ($klassen as $klas){
-    echo "<option value=$klas>$klas</option>";
-}
+ 
+echo "voornaam: ".$_POST['1stname'] . "<br>";
+echo "achternaam: ".$_POST['lastname'] . "<br>";
+echo "klas: ".$_POST['klas'] . "<br>";
+echo "leeftijd: ".$_POST['leeftijd'] . "<br>";
+echo "straat: ".$_POST['straat'] . "<br>";
+echo "plaats: ".$_POST['plaats'] . "<br>";
+echo $_POST['kleur'];
+
+if (isset($_POST['geslacht']))
+  {
+$Gender = $_POST["geslacht"];
+
+echo $Gender;
+  }
 ?>
-</select>
-<input type="submit" value="Submit">
-</form>
